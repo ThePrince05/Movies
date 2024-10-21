d03f0ddcd4a04094360e3c4d3771ede28e0de9a9
Prince Sithole <princesithole49@gmail.com>
2024-10-21 20:12:58 +0200

    Started working on logic of application.

--- app/src/main/java/com/example/movies/LoginActivity.java
+++ app/src/main/java/com/example/movies/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.movies;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
+private EditText userEdt, passEdt;
+private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        initView();
+    }
+
+    private void initView() {
+       userEdt = findViewById(R.id.editTextText);
+       passEdt = findViewById(R.id.editTextPassword);
+       loginBtn = findViewById(R.id.loginBtn);
+
+       loginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if(userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()){
+                   Toast.makeText(LoginActivity.this, "Please fill in your details", Toast.LENGTH_SHORT).show();
+
+                }
+               else if(userEdt.getText().toString().equals("test") && passEdt.toString().equals("test")){
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+               }
+               else{
+                   Toast.makeText(LoginActivity.this, "Your details are incorrect", Toast.LENGTH_SHORT).show();
+
+               }
+           }
+       });
     }
 }
--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -1,18 +1,8 @@
 package com.example.movies;
 
-
-
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,31 +10,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
     }
-
-    public void Convert(View view) {
-        try {
-
-
-        } catch (Exception exception) {
-
-        }
-    }
-
-
 }
-
-
-
